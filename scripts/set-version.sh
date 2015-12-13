f1c44c065ae1c94d5dd7657bde5d660f3495bb6b
Sebastian Menski <sebastian@menski.org>
2015-12-13 13:13:54 +0100

    feat(version): add Camunda BPM Platform 7.3.1-ee images

--- scripts/set-version.sh
+++ scripts/set-version.sh
@@ -5,4 +5,4 @@ set -e
 VERSION=${1:?"Usage: $0 VERSION"}
 
 sed -i "s/ARG VERSION=[0-9].[0-9].[0-9]/ARG VERSION=${VERSION}/g" */Dockerfile
-sed -i "s!\(menski/camunda-bpm-platform:.*\)-[0-9].[0-9].[0-9]!\1-${VERSION}!g" docker-compose.yml
+sed -i "s!\(registry.camunda.com/camunda-bpm-platform:.*\)-[0-9].[0-9].[0-9]-ee!\1-${VERSION}-ee!g" docker-compose.yml
